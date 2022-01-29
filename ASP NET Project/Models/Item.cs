6385ab29c350418cd4904808238c398b5fe17ea1
Tomasz Czarzbon <tomques11@gmail.com>
2022-01-29 17:28:34 +0100

    item, createcshtml

--- ASP NET Project/Models/Item.cs
+++ ASP NET Project/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ASP_NET_Project.Models
         public int Id { get; set; }
         public string Borrower { get; set; }
         public string Lender { get; set; }
+        [DisplayName("Item Name")]
         public string ItemName { get; set; }
     }
 }
