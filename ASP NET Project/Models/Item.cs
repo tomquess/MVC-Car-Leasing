3da4c8bef246515e83fa982f8450ed1d47e78e2e
Tomasz Czarzbon <tomques11@gmail.com>
2022-01-29 16:41:23 +0100

    lender, item name implementation

--- ASP NET Project/Models/Item.cs
+++ ASP NET Project/Models/Item.cs
@@ -11,5 +11,7 @@ namespace ASP_NET_Project.Models
         [Key]
         public int Id { get; set; }
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
     }
 }
