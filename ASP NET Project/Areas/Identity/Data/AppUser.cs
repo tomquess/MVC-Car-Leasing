1ce97b78a57cae94ae97faec9394bdd92c9b8e2d
Tomasz Czarzbon <tomques11@gmail.com>
2022-01-30 21:05:28 +0100

    login

---
+++ ASP NET Project/Areas/Identity/Data/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ASP_NET_Project.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AppUser class
+    public class AppUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="varchar(100)")]
+        public string FirstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string LastName { get; set; }
+    }
+}
